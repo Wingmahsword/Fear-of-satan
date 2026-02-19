9ed56be3da71737499d2d3a06e91c6fabc7f9d7e
Yash <yashsharma8892@gmail.com>
2026-02-19 19:26:34 +0530

    Fix ShaderPass import: Add missing import for post-processing
    - Added ShaderPass import from three/addons/postprocessing/ShaderPass.js
    - Required for custom post-processing effects
    - Enables chromatic aberration, vignette, and grain effects

--- three-post-processing.js
+++ three-post-processing.js
@@ -6,6 +6,7 @@
 import * as THREE from 'three';
 import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
 import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
+import { ShaderPass } from 'three/addons/postprocessing/ShaderPass.js';
 import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
 
 class PostProcessingPipeline {
